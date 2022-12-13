c2e5b3d77c6585acce4570bcead3dc161d2493d3
Vaishnavi Dontha <77274387+VaishnaviDontha@users.noreply.github.com>
2022-12-13 14:42:10 -0500

    Launching Broowser via Selenide and Few Checks

--- src/test/java/SelenideSampleTestScripts/LaunchingBrowser.java
+++ src/test/java/SelenideSampleTestScripts/LaunchingBrowser.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.codeborne.selenide.WebDriverRunner;
+
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
@@ -17,11 +19,18 @@ public class LaunchingBrowser {
         $(By.name("password")).setValue("SuperSecretPassword!"); 
         $(By.xpath("//button[@type='submit']")).click();
 
+        // verify text
         $(By.cssSelector("#flash")).shouldHave(text("You logged into a secure area!"));
         
+        // verify url contains
+        String url = WebDriverRunner.url();
+        Assert.assertTrue(url.contains("secure"));
 
+        // verify title
         String title = title();
         Assert.assertEquals(title, "The Internet");
+
+
     }
 
 
@@ -29,10 +38,8 @@ public class LaunchingBrowser {
     public void googleSearch() {
         open("https://www.google.com/");
     
-        $(By.cssSelector("input[title='Search']")).setValue("Selenide"); 
-        $(By.cssSelector("input[title='Search']")).sendKeys(Keys.ENTER);
+        $(By.cssSelector("input[title='Search']")).setValue("Selenide").sendKeys(Keys.ENTER);
         
-
         String title = title();
         Assert.assertEquals(title, "Selenide - Google Search");
     }
